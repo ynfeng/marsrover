9a311de7e13d0f6a09acf3878defeea1ff9a0aeb
FengYannan <fengyannan@mintechai.com>
2019-08-31 16:23:33 +0800

    Default Changelist

--- src/main/java/PosistionSensor.java
+++ src/main/java/PosistionSensor.java
@@ -4,10 +4,7 @@
 public class PosistionSensor {
 
     public boolean isInWorkArea(Area area, Position position) {
-        if (position.x() >= 0 && position.x() <= area.x()
-            && position.y() >= 0 && position.y() <= area.y()) {
-            return true;
-        }
-        return false;
+        return position.x() >= 0 && position.x() <= area.x()
+            && position.y() >= 0 && position.y() <= area.y();
     }
 }
