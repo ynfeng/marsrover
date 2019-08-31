1cbff3d02a7b9cd9a4671966e668ac621378c0ca
FengYannan <fengyannan@mintechai.com>
2019-08-31 16:22:30 +0800

    Default Changelist

--- src/test/java/MarsRoverTest.java
+++ src/test/java/MarsRoverTest.java
@@ -199,7 +199,6 @@ public class MarsRoverTest {
         marsRover.deploy(Position.of(0, 0, Direction.EAST));
         marsRover.forward();
         marsRover.forward();
-        marsRover.forward();
         Position position = marsRover.forward();
         assertThat(position, is(Position.of(1, 0, Direction.EAST)));
     }
