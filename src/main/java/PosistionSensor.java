32fc9d401bde46e555628de4d91d7c7d4e5c1067
FengYannan <fengyannan@mintechai.com>
2019-08-31 16:11:22 +0800

    Default Changelist

---
+++ src/main/java/PosistionSensor.java
@@ -0,0 +1,8 @@
+/**
+ * @author fynwin@gmail.com
+ */
+public class PosistionSensor {
+    public boolean isInWorkArea(Area area, Position position) {
+        return true;
+    }
+}
---
+++ src/test/java/PosistionSensorTest.java
@@ -0,0 +1,18 @@
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.CoreMatchers.is;
+
+import org.junit.Test;
+
+/**
+ * @author fynwin@gmail.com
+ */
+public class PosistionSensorTest {
+
+    @Test
+    public void should_detection_work_area() {
+        PosistionSensor posistionSensor = new PosistionSensor();
+        boolean result = posistionSensor.isInWorkArea(Area.of(10, 10), Position.of(2, 3, Direction.WEST));
+        assertThat(result, is(true));
+    }
+
+}
